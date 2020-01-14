b1ace124c4501fe32e6bace41d5bfb245bb69b70
Michael Lynch <michael@michaelrlynch.com>
2020-01-14 02:46:35 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,5 +1,5 @@
 #Write migration to add column to existing table shows called season, datatype string
-class AddSeasontoShows < ActiveRecord::Migration[5.2]
+class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :season, :string
   end
